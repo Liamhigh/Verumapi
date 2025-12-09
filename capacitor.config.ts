31de36fae0b40359adb4a440f2e8f8d849e91fa9
Liamhigh <Liamhigh78@gmail.com>
2025-12-09 10:27:33 +0200

    Add Android project from Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.verumomnis.app',
+  appName: 'Verum Omnis',
+  webDir: 'dist'
+};
+
+export default config;
